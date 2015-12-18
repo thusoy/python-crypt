a2d5d71cf8a752da4bce8b59f7174a28d2472b25
Tarjei Husøy <git@thusoy.com>
2015-12-18 21:20:19 +0100

    Add tox.ini and setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+
+from setuptools import setup
+import sys
+
+install_requires = []
+
+if sys.version_info < (2, 7, 0):
+    install_requires.append('argparse')
+
+setup(
+    name='pcrypt',
+    version='1.0.0',
+    author='Tarjei Husøy',
+    author_email='git@thusoy.com',
+    url='https://github.com/thusoy/python-crypt',
+    description="Pure-python implementation of the SHA2 crypt(3) functions",
+    modules=['pcrypt'],
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+            'pcrypt = pcrypt:cli',
+        ]
+    },
+    classifiers=[
+        # 'Development Status :: 1 - Planning',
+        # 'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
