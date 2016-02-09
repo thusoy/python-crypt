0408609c928b88d2f5076101694385a64f7fc6ee
Tarjei Husøy <git@thusoy.com>
2016-02-08 22:39:52 -0800

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,16 @@ install_requires = []
 if sys.version_info < (2, 7, 0):
     install_requires.append('argparse')
 
+# Create a long description for PyPI, but don't fail if run under tox
+# and the files are not found
+long_description = []
+try:
+    for doc_file in ('README.md', 'CHANGELOG.md'):
+        with open(doc_file) as fh:
+            long_description.append(fh.read())
+except:
+    print('Failed to add long_description, ignoring.')
+
 setup(
     name='pcrypt',
     version='1.0.3',
@@ -16,6 +26,7 @@ setup(
     author_email='git@thusoy.com',
     url='https://github.com/thusoy/python-crypt',
     description="Pure-python implementation of the SHA2 crypt(3) functions",
+    long_description='\n'.join(long_description),
     py_modules=['pcrypt'],
     install_requires=install_requires,
     extras_require={
